5d6598336f3490caabd146096715db32b43de6d8
Lina Budkova <linabudkova35@gmail.com>
2022-12-06 09:13:51 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,23 +1,12 @@
+
 const listEl = document.querySelector('#categories');
 
-console.log('Number of categories: ', listEl.children.length);
+const numberOfCategoriesEl = listEl.children.length;
+console.log(`Number of categories: ${numberOfCategoriesEl}`);
 
 const itemEl = document.querySelectorAll('.item');
 
-const titleEl1 = itemEl[0].querySelector('h2');
-console.log('Category: ', titleEl1.textContent);
-
-const listEl1 = itemEl[0].querySelectorAll('li');
-console.log('Elements: ', listEl1.length);
-
-const titleEl2 = itemEl[1].querySelector('h2');
-console.log('Category: ', titleEl2.textContent);
-
-const listEl2 = itemEl[1].querySelectorAll('li');
-console.log('Elements: ', listEl2.length);
-
-const titleEl3 = itemEl[2].querySelector('h2');
-console.log('Category: ', titleEl3.textContent);
-
-const listEl3 = itemEl[2].querySelectorAll('li');
-console.log('Elements: ', listEl3.length);
+itemEl.forEach(function (item) {
+  console.log('Category: ', item.firstElementChild.textContent);
+  console.log('Elements: ', item.lastElementChild.children.length);
+});
